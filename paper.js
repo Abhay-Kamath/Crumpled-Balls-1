2f1cdf486a0b0dab3f3862fc7ba4998ebf3ce05a
Abhay Kamath <puttur.abhay.kamath@gmail.com>
2020-11-29 16:54:37 +0530

    Adding Images for paper and dustbin

--- paper.js
+++ paper.js
@@ -3,12 +3,13 @@ class paper{
         this.body = Bodies.circle(x,y,radius,{isStatic:false,density:1.2,restitution:0.3,friction:0.5})
         World.add(world,this.body);
         this.radius = radius;
+        this.image = loadImage("paper.png")
     }
     display(){
         push()
         fill("blue")
-        rectMode(CENTER);
-        ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius);
+        imageMode(CENTER);
+        image(this.image,this.body.position.x,this.body.position.y,this.radius,this.radius);
         pop()
     }
 }
--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ const Body = Matter.Body;
 
 function preload()
 {
-	
+	dustbin = loadImage("dustbingreen.png")
 }
 
 function setup() {
@@ -23,26 +23,27 @@ function setup() {
 	c1 = new stick(800,595,150,20);
 	c2 = new stick(725,540,20,100);
 	c3 = new stick(875,540,20,100);
-	paper1 = new paper(100,580,40)
-	ground1 = new stick(500,590,1000,20)
+	paper1 = new paper(100,360,50)
+	ground1 = new stick(500,640,1000,20)
 }
 
 
 function draw() {
 	Engine.update(engine);
   rectMode(CENTER);
-  background(0);
+  background("white");
   drawSprites();
   c1.display();
   c2.display();
   c3.display();
   paper1.display();
-  
+  imageMode(CENTER)
+  image(dustbin,800,500,185,200)
 }
 function keyPressed(){
 	if (keyCode === UP_ARROW) {
 		console.log(paper1.body)
-	  Matter.Body.applyForce(paper1.body,paper1.body.position,{x:285,y:-255});
+	  Matter.Body.applyForce(paper1.body,paper1.body.position,{x:485,y:-455});
 	  
 	}
   }
